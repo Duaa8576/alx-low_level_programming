03a308083062c424d3127eb7a758ff2cfc217016
Duaa8576 <duaaaaa51@gmail.com>
2023-12-15 03:18:32 -0800

    hi

---
+++ 0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to int
+ * @b: pointer
+ * Return: Success
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int bi = 0;
+
+	if (!b)
+		return (0);
+
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+
+		bi <<= 1;
+		bi += *b - '0';
+		b++;
+	}
+
+	return (bi);
+}
