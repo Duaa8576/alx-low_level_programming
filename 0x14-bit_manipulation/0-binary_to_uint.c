dbaf03cd14bb93f18ba432183fec0a34b673bdee
Duaa8576 <duaaaaa51@gmail.com>
2023-12-15 03:33:52 -0800

    zx

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,29 +8,21 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int uint, place;
-	int n;
+	unsigned int bi = 0;
 
 	if (!b)
 		return (0);
 
-	for (n = 0; b[n]; n++)
-		;
-	n--;
-	for (place = 1, uint = 0; n >= 0; n--)
+	while (*b)
 	{
-		if (b[n] == '0')
-		{
-			place *= 2;
-			continue;
-		}
-		else if (b[n] == '1')
-		{
-			uint += place;
-			place *= 2;
-			continue;
-		}
-		return (0);
+		if (*b != '0' && *b != '1')
+			return (0);
+
+		bi <<= 1;
+		bi += *b - '0';
+		b++;
 	}
-	return (uint);
+
+	return (bi);
 }
+
